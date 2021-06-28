9ec9cc2a982c2eae961312f5b12aec08c4c90e6d
mchrupcala <michael.chrupcala@gmail.com>
2021-06-28 07:27:11 -0400

    Copied over template from michael's org

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Link from 'next/link';
+import Blob from "../components/Blob";
+
+const Layout = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Layout;
