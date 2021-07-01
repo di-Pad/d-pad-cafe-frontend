7b6abd1c66976caec176ad48faccbdb9bcd6617b
mchrupcala <michael.chrupcala@gmail.com>
2021-07-01 11:41:33 -0400

    Pages created for integrate web3 and design. Integrate page basics finished

---
+++ pages/design/index.js
@@ -0,0 +1,7 @@
+const Design = () => {
+    return (
+        <div>design</div>
+    )
+}
+
+export default Design;
--- pages/integrate/index.js
+++ pages/integrate/index.js
@@ -1,8 +1,104 @@
 const Integrate = () => {
     return (
-        <div>
-                Hi!
+    <div className="container">
+
+      <main className="landing-main">
+        <div className="landing-sidebar">
+          <h2>Welcome to your<span className="heavy"> Partner Agreement!</span></h2>
+
+          <img src="/d-pad-logo.png"></img>
+
+          <p>In just two steps, you will integrate a <b>universal, sybil-resistant login</b>
+             for your users - and automate <b>an internal, mathematically-fair Tokenomics</b>
+             for your community.
+          </p>
         </div>
+
+        <div className="integrate-content">
+          <div>
+            <h2 className="heavy">Partner's Agreement</h2>
+            <h4>Select the template that best represents your project / protocol.</h4>
+          </div>
+
+          <div className="integrate-template-content">
+            <div className="integrate-project-types">
+                <div className='white-card'>
+                    <div className="top-card">
+                        <img className="image-7" src='opensource-defi-black.png' alt="card-logo"/>
+
+                        <div className="title-white-card raleway-bold-alto-22px">
+                            <>
+                        Open-Source & DeFi
+                            </>
+                        </div>
+                    </div>
+
+                    <div className="description-white-card raleway-normal-alto-18px">
+                        For researchers & web3, open-source teams, that innovate in a liberal fashion - for a more sustainable, meritocratic world.
+                    </div>
+
+                    <img className="line-26" src='/geometric-card-line-break.png' alt="line" />
+                </div>
+
+                <div className="white-card">
+                    <div className="top-card">
+                        <img className="image-7" src='opensource-defi-black.png' alt="card-logo"/>
+
+                        <div className="title-white-card raleway-bold-alto-22px">
+                            <>
+                        Art, Events & NFTs
+                            </>
+                        </div>
+                    </div>
+
+                    <div className="description-white-card raleway-normal-alto-18px">
+                    Art movements, writers & creatives of all kind who use Art & provable ownership for purer forms of human interaction.
+                    </div>
+
+                    <img className="line-26" src='/geometric-card-line-break.png' alt="line" />
+                </div>
+
+                <div className="white-card">
+                <div className="top-card">
+                    <img className="image-7" src='opensource-defi-black.png' alt="card-logo"/>
+
+                    <div className="title-white-card raleway-bold-alto-22px">
+                        <>
+                    Local Projects & DAOs
+                        </>
+                    </div>
+                </div>
+
+                <div className="description-white-card raleway-normal-alto-18px">
+                From support for people in need, to innovative local hubs to get together & create something greater than oneself.
+                </div>
+
+                <img className="line-26" src='/geometric-card-line-break.png' alt="line" />
+                </div>
+            </div>
+
+            <div className="bootstrap-button">
+                <p>Bootstrap your Community Economy</p>
+            </div>
+
+            <div className="integrate-button-panel">
+                <button>
+                Start from Scratch
+                </button>
+
+                <button>
+                Import your Contract
+                </button>
+            </div>
+
+            <button className="integrate-deploy">
+                Sign & Deploy 🚀
+            </button>
+
+          </div>
+        </div>
+      </main>
+    </div>
     )
 }
 
