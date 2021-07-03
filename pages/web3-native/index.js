8faa0ba72b5b54a4f74f4fb7d01d9fe6cb435868
mchrupcala <michael.chrupcala@gmail.com>
2021-07-03 10:32:24 -0400

    added calendar to profit

--- pages/web3-native/index.js
+++ pages/web3-native/index.js
@@ -1,19 +1,23 @@
 import Image from 'next/image';
+import Calendar from 'react-calendar';
+import { useState } from 'react';
+import 'react-calendar/dist/Calendar.css';
 
 const Web3Native = () => {
+    const [value, onChange] = useState(new Date());
 
-    // the fixed width for the Image tag on line 18 will be a problem......
+
     return (
         <div className="container">
   
         <main className="landing-main">
-          <div className="landing-sidebar">
+          <div className="profit-sidebar">
             <div className="user-header">
                 <Image src="/d-pad-logo.png" width="55" height="40"></Image>
                 <h4>_username_</h4>
             </div>
 
-            <Image className="line-26" src='/geometric-card-line-break.png' alt="line" width="300" height="2"/>
+            <Image className="line-break" src='/geometric-card-line-break.png' alt="line" width="300" height="2"/>
 
             <div className="profit-sidebar-buttons">
                 <div className="pill">
@@ -24,7 +28,7 @@ const Web3Native = () => {
 
                 <div className="pill contracts-pill">
                 <Image src="/d-pad-logo.png" width="55" height="40"/>
-                    <div class="listed-contracts">
+                    <div className="listed-contracts">
                         <h4>Listed Contracts</h4>
                         <form>
                             <input placeholder="0x..." ></input>
@@ -58,7 +62,7 @@ const Web3Native = () => {
 
             <div className="profit-sharing-agreement">
                 <div className="profit-sharing-controls">
-                    <div>
+                    <div className="control-section">
                         <h3>Distribution Rate</h3>
 
                         <p>Tot. Profit (%) to share:</p>
@@ -67,32 +71,39 @@ const Web3Native = () => {
                         <p>How often would you like the Profit-Sharing distribution to happen? No worries, we&#39;ll optimize if for you,
                             based on the size & growth rate of your Community :)
                         </p>
+
+                        <div className="distribution-rate-buttons">
+                            <button>Weekly</button>
+                            <button>Monthly</button>
+                        </div>
                     </div>
 
-                    <div>
+                    <div className="control-section">
                         <h3>Interactions' Proximity Level</h3>
 
                         <p>Distribute your Token based on the proximity of the interactions to your protocol!</p>
 
-                        <button>Only to your Protocol</button>
+                        <button className="proximity-button">Only to your Protocol</button>
 
-                        <button>To the same Template</button>
+                        <button className="proximity-button">To the same Template</button>
 
-                        <button>All SkillWallet holders</button>
+                        <button className="proximity-button">All SkillWallet holders</button>
                     </div>
 
-                    <div>
+                    <div className="control-section">
                         <h3>Contract Start Date</h3>
 
-                        <div>Calendar here this is a calendar where you will be able to choose dates in a year.</div>
+                        <Calendar 
+                        onChange={onChange}
+                        value={value}/>
                     </div>
                 </div>
 
                 <h3 style={{fontStyle: "underline", fontWeight: "bold"}}>Bootstrap your Profit-Sharing Community</h3>
 
-                <button>Click to Deploy your Treasury Contract</button>
+                <button className="deploy-button">Click to Deploy your Treasury Contract</button>
 
-                <button className="profit-share">Start Sharing 🚀</button>
+                <button className="profit-share" onClick={() => console.log(value.getMonth())}>Start Sharing 🚀</button>
             </div>
 
           </div>
