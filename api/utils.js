09e8e98aeffd13680d0be39c13c3671d55408dae
mchrupcala <michael.chrupcala@gmail.com>
2021-07-13 16:27:19 -0400

    fixed the POST request for nonce

--- api/utils.js
+++ api/utils.js
@@ -1,8 +1,7 @@
 export const getSkillWalletNonce = async () => {
-    const response = await fetch('https://api.skillwallet.id/api/skillwallet/-1/nonces', {
+    const response = await fetch('https://api.skillwallet.id/api/skillwallet/-1/nonces?action=1', {
         method: 'POST'
     })
     const nonce = await response.json();
-    console.log(nonce);
-    return nonce;
+    return nonce.nonce;
 };
